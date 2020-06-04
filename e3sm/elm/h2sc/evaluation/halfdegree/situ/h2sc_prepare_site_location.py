dc7124d0dc1845f4cc275b2844becf804e1ffc8a
Chang Liao <changliao.climate@gmail.com>
2020-06-03 17:07:22 -0700

    add location map

---
+++ e3sm/elm/h2sc/evaluation/halfdegree/situ/h2sc_prepare_site_location.py
@@ -0,0 +1,9 @@
+#this script is used to prepre the location of all in situ datasest
+
+def h2sc_prepare_site_location():
+
+    #we will convert all location to wgs 84 first?
+    #also because map projection is better in IDL, please use the IDL libarary to details
+    return
+if __name__ == '__main__':    
+    h2sc_prepare_site_location()
