4aa37a50685b84bbb148db504a51ebc763ea7071
Chang Liao <changliao1025@gmail.com>
2020-11-20 11:35:40 -0800

    update save vtk

--- pye3sm/elm/general/halfdegree/plot/elm_surface_plot_variable_halfdegree_domain.py
+++ pye3sm/elm/general/halfdegree/plot/elm_surface_plot_variable_halfdegree_domain.py
@@ -14,6 +14,9 @@ from pyes.gis.gdal.read.gdal_read_envi_file import gdal_read_envi_file, gdal_rea
 from pyes.gis.gdal.read.gdal_read_geotiff_file import gdal_read_geotiff_file, gdal_read_geotiff_file_multiple_band
 
 
+from pyes.visual.surface.convert_array_to_vtk_polygon import convert_array_to_vtk_polygon
+
+
 sPath_pye3sm = sWorkspace_code +  slash + 'python' + slash + 'e3sm' + slash + 'pye3sm'
 sys.path.append(sPath_pye3sm)
 from pye3sm.shared.e3sm import pye3sm
@@ -102,15 +105,15 @@ def elm_surface_plot_variable_halfdegree_domain(oE3SM_in,\
         dummy = gdal_read_geotiff_file(sFilename_basin)
         dummy_mask1 = dummy[0]
         sLabel_legend = sDomain.title()               
-        pShape = aVariable_total_subset.shape
-        aVariable1= np.full(pShape, 0, dtype=float)
+        #pShape = aVariable_total_subset.shape
+        #aVariable1= np.full(pShape, 0, dtype=float)
         #for i in np.arange(0, pShape[0], 1):
         #    aVariable1[i, :,:] = aVariable_total_subset[i, :,:]
         #    aVariable1[i][dummy_mask1!=1] = missing_value
         #    pass
 
-        aVariable2 = aVariable1[5:,:,:]
-        aVariable2 = aVariable2[0].reshape(nrow , ncolumn)
+        aVariable2 = aVariable_total_subset[5,:,:]
+        aWTD = np.reshape(aVariable2,  (nrow , ncolumn) )
        
         sFilename_out = sWorkspace_analysis_case_domain + slash \
             + sVariable + '_' + sDomain + '_surface_plot'
@@ -119,146 +122,52 @@ def elm_surface_plot_variable_halfdegree_domain(oE3SM_in,\
         b = np.full((nrow, ncolumn), np.nan, dtype=float)
         c = np.full((nrow, ncolumn), np.nan, dtype=float)
 
-        b[a] = aVariable2[a]        
+        b[a] = aWTD[a]        
         c[a] = aDem[a]
         #range of lat/lon
-        lat_min = np.min(a[0])
-        lat_max = np.max(a[0])
-        lon_min = np.min(a[1])
-        lon_max = np.max(a[1])
-        aGrid_x = grid_x[ lat_min:lat_max, lon_min:lon_max  ]
-        aGrid_y = grid_y[ lat_min:lat_max, lon_min:lon_max  ]
+        row_min = np.min(a[0])
+        row_max = np.max(a[0])
+        column_min = np.min(a[1])
+        column_max = np.max(a[1])
+        aGrid_x = grid_x[ row_min:row_max, column_min:column_max  ]
+        aGrid_y = grid_y[ row_min:row_max, column_min:column_max  ]
 
 
-        aData = b[ lat_min:lat_max, lon_min:lon_max  ]
-        aDem_domain = c[ lat_min:lat_max, lon_min:lon_max  ]
+        pShape = aGrid_x.shape
+        aData = b[ row_min:row_max, column_min:column_max  ]
 
-        Data = aDem_domain - aData
+        aDem_suface = c[ row_min:row_max, column_min:column_max  ]
+        #aDem_suface[np.where(aDem_suface > 500)] = 500
+        aDem_wt = aDem_suface - aData
 
-        print(np.min(aGrid_x), np.max(aGrid_x), np.min(aGrid_y), np.max(aGrid_y))
+        nlayer=2
+        aDem_all = np.full((nlayer, pShape[0], pShape[1] ), np.nan, dtype=float)
+        aWTD_all = np.full((nlayer, pShape[0], pShape[1]), np.nan, dtype=float)
+        
+        aDem_all[0,:,:] = aDem_suface
+        #aDem_all[1,:,:] = -9999
+        #aWTD_all[0,:,:] = -9999
+        aWTD_all[1,:,:] = aData
 
-        #we will output vtk from now on because Pyvista cannot provide enough capability
-        aDem_domain[np.where(aDem_domain > 500)] = 500
-        aDem_domain= aDem_domain* 0.01
-        aData = aData * 0.01
+   
 
-        ncolumn1, nrow1, nlayer = len(aGrid_x[1]), len(aGrid_y), 2
-        lx, ly = (np.nanmax(aGrid_x)-np.nanmin(aGrid_x)), (np.nanmax(aGrid_y)-np.nanmin(aGrid_y))
-        dx, dy = 0.5 , 0.5
-        ncells = ncolumn1 * nrow1 * nlayer
-        npoints = (ncolumn1 + 1) * (nrow1 + 1) * (nlayer + 1)
+        pArray_in = [aDem_all, aWTD_all]
+        pZ_in = np.full((3, pShape[0], pShape[1]), 0.0, dtype=float)
+        dummy = aDem_suface
+        nan_index = np.where( np.isnan(dummy) == True )
+        
+       
+        pZ_in[0, :,:] = np.log(aDem_suface)
+        pZ_in[1,:,:] = np.log(aDem_wt) - 10
+        pZ_in[2,:,:] = 0.0
 
-        X = np.arange(np.nanmin(aGrid_x) - 0.25, np.nanmax(aGrid_x) + dx, dx, dtype='float64')
-        Y = np.arange(np.nanmax(aGrid_y) +0.25, np.nanmin(aGrid_y) - dy, -dy, dtype='float64')
-        # Coordinates
-        x = np.zeros(( nlayer + 1,nrow1 + 1,ncolumn1 + 1))
-        y = np.zeros(( nlayer + 1 ,nrow1 + 1,ncolumn1 + 1))
-        #z = np.zeros(( nlayer + 1, nrow1 + 1,ncolumn1 + 1))
-        z = np.full( ( nlayer + 1, nrow1 + 1,ncolumn1 + 1), np.nan, dtype=float   )
-
-        #right edge z coordinates for top layer
-        for iRow in range(nrow1 ):
-            #z[0,iRow,ncolumn1] = aDem_domain[iRow, ncolumn1-1]
-            z[2,iRow+1,ncolumn1] = aDem_domain[iRow, ncolumn1-1]
-            pass
-
-        #bottom line
-        for iColumn in range(ncolumn1 ):
-            #z[0,nrow1,iColumn] = aDem_domain[nrow1-1, iColumn]
-            z[2,nrow1,iColumn] = aDem_domain[nrow1-1, iColumn]
-            pass
-
-        z[0,nrow1,ncolumn1] = aDem_domain[nrow1-1, ncolumn1-1]
-
-        #right edge
-        for iRow in range(nrow1 ):
-            z[1,iRow,ncolumn1] = aDem_domain[iRow, ncolumn1-1] - aData[iRow, ncolumn1-1]-3 
-            #additional space added
-            pass
-
-        #bottom line
-        for iColumn in range(ncolumn1 ):
-            z[1,nrow1,iColumn] = aDem_domain[nrow1-1, iColumn]- aData[iRow, ncolumn1-1] -3
-            pass
-
-        z[1,nrow1,ncolumn1] = aDem_domain[nrow1-1, ncolumn1-1]  - aData[iRow, ncolumn1-1]-3
-
-
-        for iRow in range(nrow1 + 1):
-            for iColumn in range(ncolumn1 + 1):
-
-                x[0,iRow,iColumn] = X[iColumn]
-                y[0,iRow,iColumn] = Y[iRow]
-                if (iColumn < ncolumn1 and iRow < nrow1):
-                    z[0,iRow,iColumn] = aDem_domain[iRow, iColumn]
-                    pass
-
-        for iRow in range(nrow1 + 1):
-            for iColumn in range(ncolumn1 + 1):
-
-                x[1,iRow,iColumn] = X[iColumn]
-                y[1,iRow,iColumn] = Y[iRow]
-                if (iColumn < ncolumn1 and iRow < nrow1):
-                    z[1,iRow,iColumn] = aDem_domain[iRow, iColumn]- aData[iRow, iColumn]-3
-
-                    pass
+        pLabel = np.array(['ele', 'wtd'])
         
 
-        #bottom
-        for iRow in range(nrow1 + 1):
-            for iColumn in range(ncolumn1 + 1):
-
-                x[2,iRow,iColumn] = X[iColumn]
-                y[2,iRow,iColumn] = Y[iRow]
-                z[2,iRow,iColumn] = np.nanmin(  aDem_domain - aData ) -5
+        convert_array_to_vtk_polygon( pArray_in,pLabel, aGrid_x, aGrid_y, pZ_in, sFilename_out   )
 
 
-
-        # Define connectivity or vertices that belongs to each element
-        conn = np.full((nrow1 * ncolumn1 * nlayer * 4), 0, dtype=int)
-
-        for iRow in range(nrow1):
-            for iColumn in range(ncolumn1 ):
-                start_index = 0 * nrow1 * ncolumn1 * 4
-                new_index = iRow * ncolumn1 * 4 + iColumn * 4
-                conn[start_index + new_index] = iRow * (ncolumn1+1) + iColumn
-                conn[start_index + new_index+1] = iRow * (ncolumn1+1) + iColumn+1
-                conn[start_index + new_index+2] = (iRow+1) * (ncolumn1+1) + iColumn+1
-                conn[start_index + new_index+3] = (iRow+1) * (ncolumn1+1) + iColumn
-        for iRow in range(nrow1 ):
-            for iColumn in range(ncolumn1 ):
-
-                    start_index = 1 * nrow1 * ncolumn1 * 4
-                    new_index = iRow * ncolumn1 * 4 + iColumn * 4
-                    conn[start_index + new_index]  = iRow * (ncolumn1+1) + iColumn + (ncolumn1+1)*(nrow1+1)
-                    conn[start_index + new_index+1]  = iRow * (ncolumn1+1) + iColumn+1 + (ncolumn1+1)*(nrow1+1)
-                    conn[start_index + new_index+2]  = (iRow+1) * (ncolumn1+1) + iColumn+1 + (ncolumn1+1)*(nrow1+1)
-                    conn[start_index + new_index+3]  = (iRow+1) * (ncolumn1+1) + iColumn + (ncolumn1+1)*(nrow1+1)
-
-
-        #pressure = np.random.rand(ncells).reshape( (nx, ny, nz))
-        ele1 = np.full( ncells, np.nan, dtype=float )
-        wtd1 = np.full( ncells, np.nan, dtype=float )
-        ele1[0:nrow1* ncolumn1] = np.reshape(aDem_domain, nrow1* ncolumn1)
-        wtd1[ nrow1* ncolumn1: ncells] = np.reshape(aData, nrow1* ncolumn1)
-        #ele1=np.reshape(aDem_domain, nrow1* ncolumn1)
-        #wtd1=np.reshape(aData, nrow1* ncolumn1)
-
-        # Define offset of last vertex of each element
-        offset = np.arange( 4, (4*ncolumn1*nrow1*nlayer+1), 4 )
-
-
-
-        # Define cell types
-
-        ctype = np.full( (nrow1, ncolumn1, nlayer), VtkQuad.tid )
-
-        conn= np.reshape(conn, (4*nrow1*ncolumn1*nlayer))
-        x.shape= (nrow1+1) * (ncolumn1+1) * (nlayer+1)
-        y.shape= (nrow1+1) * (ncolumn1+1) * (nlayer+1)
-        z.shape= (nrow1+1) * (ncolumn1+1) * (nlayer+1)
-        unstructuredGridToVTK(sFilename_out, x, y, z, connectivity = conn, \
-                              offsets = offset, cell_types = ctype, cellData = {'ele': ele1,"wtd" : wtd1}, pointData = None)
+        
         print('finished')
 
 
