aacef9f43d3977dbf9b928419af848cda481958d
changliao1025 <changliao1025@outlook.com>
2021-09-08 16:56:41 -0700

    add basin search function

--- pye3sm/mosart/grid/create_global_river_mosart_information.py
+++ pye3sm/mosart/grid/create_global_river_mosart_information.py
@@ -2,9 +2,8 @@ import os
 import numpy
 import numpy as np
 from netCDF4 import Dataset
-from numpy.lib.shape_base import _put_along_axis_dispatcher
 from pyearth.toolbox.reader.text_reader_string import text_reader_string
-def create_global_river_mosart_information(sFilename_csv):
+def create_global_river_mosart_information(sFilename_csv, sFilename_out):
 
     aRiver = text_reader_string(sFilename_csv, iSkipline_in =1, cDelimiter_in=',')
     nriver = len(aRiver)
@@ -21,15 +20,17 @@ def create_global_river_mosart_information(sFilename_csv):
     print(aDatasets.variables.keys() )
     #output file
 
+    ofs = open(sFilename_out, 'w')
+
+    
+
     # Copy variables
     for sKey, aValue in aDatasets.variables.items():
         #print(sKey, aValue)
-        print(aValue.datatype)
-        print( aValue.dimensions)
+        #print(aValue.datatype)
+        #print( aValue.dimensions)
         # we need to take care of rec dimension
 
-        # Copy variable attributes
-        #outVar.setncatts({k: aValue.getncattr(k) for k in aValue.ncattrs()})
         if sKey == 'ID':
             aID =  (aValue[:]).data
         if sKey == 'dnID':
@@ -46,35 +47,41 @@ def create_global_river_mosart_information(sFilename_csv):
 
     for i in range( nriver ):
         sRiver = aRiver[i][1].strip()
+        aRiver[i][1] = sRiver
         dDraiange = float(aRiver[i][2].strip())
         dLon = float(aRiver[i][4].strip())
         dLat = float(aRiver[i][5].strip())
 
-        if sRiver.lower() == 'amazon':
-            #search mosart parameter 
-            dummy_index = np.where( (aLongitude > (dLon-5)) & (aLongitude < (dLon+5)) \
-                & (aLatitude > (dLat-5)) & (aLatitude < (dLat+5)) )
+        
+        #search mosart parameter 
+        dummy_index = np.where( (aLongitude > (dLon-5)) & (aLongitude < (dLon+5)) \
+            & (aLatitude > (dLat-5)) & (aLatitude < (dLat+5)) )
+        
+        if len(dummy_index) > 0:
+            dummy_accu = aAccu[dummy_index]
+            max_accu = np.max(dummy_accu) 
             
-            if len(dummy_index) > 0:
-                dummy_accu = aAccu[dummy_index]
-                max_accu = np.max(dummy_accu) 
+            
+            dummy_index1 = np.where(dummy_accu == max_accu )
+            dummy_index2 = dummy_index1[0]
+            dummy_index3 = dummy_index[0][dummy_index2]
+            dummy_index4 = dummy_index[1][dummy_index2]
+            lID  = aID[ dummy_index3, dummy_index4]
+            print(sRiver, max_accu/1.0E6, dDraiange, int(lID[0])
+            
+            #print(aAccu[ dummy_index3, dummy_index4])
+            aRiver[i][6] = "{:0d}".format(int(lID[0]))
 
-                print(max_accu/ 1.0E6)
-                print(dDraiange)
+            sLine = ','.join(aRiver[i] ) + '\n'
+            ofs.write(sLine)
 
-                dummy_index1 = np.where(dummy_accu == max_accu )
-                dummy_index2 = dummy_index1[0]
-                dummy_index3 = dummy_index[0][dummy_index2]
-                dummy_index4 = dummy_index[1][dummy_index2]
-                lID  = aID[ dummy_index3, dummy_index4]
-                print(lID)
-                print(aAccu[ dummy_index3, dummy_index4])
+        else:
+            pass    
+        
+        
 
-            pass
         pass
+
+    ofs.close()
     return
 
-
-if __name__ == '__main__':
-    sFilename_csv = '/qfs/people/liao313/data/e3sm/global/global_river.csv'
-    create_global_river_mosart_information(sFilename_csv)
